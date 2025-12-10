6ebf57a824ab4fa9af3a642743a85e8b658e4dbd
stephenkyaw <kyawmyoaung.dev@gmail.com>
2025-12-11 04:01:55 +0700

    fix: github workflow for github page.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/OpenLearnAI/',
+  base: process.env.NODE_ENV === 'production' ? '/OpenLearnAI/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
